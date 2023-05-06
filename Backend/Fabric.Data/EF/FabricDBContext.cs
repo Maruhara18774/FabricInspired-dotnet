22f365ffe9560a2a262de03f668de92593fb6fa0
Linh Mac Le My <linlin.18dh110774huflit@gmail.com>
2023-05-06 23:15:27 +0700

    DBContext, Context Factory

---
+++ Backend/Fabric.Data/EF/FabricDBContext.cs
@@ -0,0 +1,46 @@
+﻿using Fabric.Data.Configurations;
+using Fabric.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabric.Data.EF
+{
+    public class FabricDBContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Moonboard> Moonboards { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Pattern> Patterns { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+
+        public FabricDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ImageConfiguration());
+            modelBuilder.ApplyConfiguration(new MoonboardConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderDetailConfiguration());
+            modelBuilder.ApplyConfiguration(new PatternConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new SaleConfiguration());
+            modelBuilder.ApplyConfiguration(new SettingConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+}
---
+++ Backend/Fabric.Data/EF/FabricDBContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Fabric.Data.EF
+{
+    public class FabricDBContextFactory : IDesignTimeDbContextFactory<FabricDBContext>
+    {
+        public FabricDBContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("LulusDatabase");
+
+            var optionBuilder = new DbContextOptionsBuilder<FabricDBContext>();
+            optionBuilder.UseSqlServer(connectionString);
+
+            return new FabricDBContext(optionBuilder.Options);
+        }
+    }
+}
